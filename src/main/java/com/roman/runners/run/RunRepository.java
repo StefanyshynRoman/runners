b18b218c9690eb5946a702a54782181ff36fc4c7
roman <romanpz051@gmail.com>
2024-07-26 18:18:08 +0200

    0.0.2.2 - h2.database

--- src/main/java/com/roman/runners/run/RunRepository.java
+++ src/main/java/com/roman/runners/run/RunRepository.java
@@ -1,6 +1,9 @@
 package com.roman.runners.run;
 
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -11,43 +14,16 @@ import java.util.Optional;
 
 @Repository
 public class RunRepository {
-    private List<Run> runs = new ArrayList<>();
+    private static final Logger log = LoggerFactory.getLogger(RunRepository.class);
+    private final JdbcClient jdbcClient;
 
-    List<Run> findAll() {
-        return runs;
-    }
-    Optional<Run> findById(Integer id) {
-        return runs.stream().filter(run -> run.id()==id).findFirst();
-    }
-    void create(Run run){
-        runs.add(run);
-    }
-    void update(Run run, Integer id){
-        Optional<Run> existingRun=findById(id);
-        if(existingRun.isPresent()){
-            runs.set(runs.indexOf(existingRun.get()),run);
-        }
+    public RunRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
     }
 
-    void delete(Integer id){
-        runs.removeIf(run -> run.id().equals(id));
+    public List<Run> findAll() {
+        return jdbcClient.sql("select * from run")
+                .query(Run.class)
+                .list();
     }
-
-    @PostConstruct
-    private void init() {
-        runs.add(new Run(1,
-                "Monday Morning Run",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
-                3,
-                Location.INDOOR));
-        runs.add(new Run(2,
-                "Wednesday Evening Run",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
-                6,
-                Location.INDOOR));
-    }
-
-
 }
